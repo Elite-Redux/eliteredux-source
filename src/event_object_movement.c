9684a3392e397fbdd1626639d9528fc9f08ebef2
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-04-27 23:50:41 +0200

    added hooded figure

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -478,6 +478,7 @@ const u8 gInitialMovementTypeFacingDirections[] = {
 #define OBJ_EVENT_PAL_TAG_SALAMENCE               0x112F
 #define OBJ_EVENT_PAL_TAG_DEWGONG                 0x1130
 #define OBJ_EVENT_PAL_TAG_FERALIGATR              0x1131
+#define OBJ_EVENT_PAL_TAG_HOODED                  0x1132
 #define OBJ_EVENT_PAL_TAG_NONE                    0x11FF
 
 #include "data/object_events/object_event_graphics_info_pointers.h"
@@ -538,6 +539,7 @@ static const struct SpritePalette sObjectEventSpritePalettes[] = {
     {gObjectEventPaletteSalamence,          OBJ_EVENT_PAL_TAG_SALAMENCE},
     {gObjectEventPaletteDewgong,            OBJ_EVENT_PAL_TAG_DEWGONG},
     {gObjectEventPaletteFeraligatr,         OBJ_EVENT_PAL_TAG_FERALIGATR},
+    {gObjectEventPaletteHooded,             OBJ_EVENT_PAL_TAG_HOODED},
     {NULL,                                  0x0000},
 };
 
