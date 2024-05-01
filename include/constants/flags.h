7cc4bb25e327f381e6eea51066d7e82d2883241a
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-05-02 00:56:50 +0200

    fixed some bits about the trainer secret

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1505,6 +1505,7 @@
 #define FLAG_HIDE_RIVAL_SOOTOPOLIS_EVERGRANDE                       3064
 #define FLAG_RIVAL_EVERGRANDE_ESCORT                                3065
 #define FLAG_HIDE_RIVAL_HELPING_WATERFALL_ROUTE_119                 3066
+#define FLAG_RIVAL_HELPED_WATERFALL_ROUTE_119                       3067
 
 #define FLAGS_COUNT                                                 6135 //Added 3000 new flags
 
