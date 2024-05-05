f35c353531db97f2cc91f7ecdfe7adbf0fc9425e
Mawootad <adinfinatioma@gmail.com>
2024-05-05 08:31:55 -0700

    Clear hitmarker at end of turn

--- src/battle_main.c
+++ src/battle_main.c
@@ -4065,6 +4065,7 @@ void BattleTurnPassed(void)
     if (HandleWishPerishSongOnTurnEnd())
         return;
 
+    ClearMiscTurnFlags();
     TurnValuesCleanUp(FALSE);
     gHitMarker &= ~(HITMARKER_NO_ATTACKSTRING);
     gHitMarker &= ~(HITMARKER_UNABLE_TO_USE_MOVE);
