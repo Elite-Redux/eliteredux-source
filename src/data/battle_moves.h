3c759f6261761f8376f0b0733fca3bc2d134e2cf
Mawootad <adinfinatioma@gmail.com>
2024-05-23 22:38:20 -0700

    Change double shock to physical

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -10933,7 +10933,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 100,
         .target = MOVE_TARGET_SELECTED,
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
-        .split = SPLIT_SPECIAL,
+        .split = SPLIT_PHYSICAL,
     },
     [MOVE_COMEUPPANCE] =
     {
