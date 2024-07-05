ef67f3b30f009af183cc7052c7b0c8014ee063f4
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-07-05 21:59:17 +0200

    added further compilations needs and made the basic of internal logic of the battle event system

--- include/battle_events.h
+++ include/battle_events.h
@@ -1,9 +1,15 @@
 #ifndef GUARD_BATTLE_EVENTS_H
 #define GUARD_BATTLE_EVENTS_H
 
+
 #define BATTLE_EVENTS_MAX_REGISTERABLE 16
 
 extern u8 gNbBattleEvents;
 extern u8 gBattleEvents[BATTLE_EVENTS_MAX_REGISTERABLE];
+void registerBattleEvent(u8 battleEvent);
+void unregisterBattlesEvents();
+// exec it whenever battle events needs to executed
+void execBattleEvents();
+void battleEventsMegaSwitch(u8 battleEvent);
 
 #endif // GUARD_BATTLE_EVENTS_H
--- src/battle_events.c
+++ src/battle_events.c
@@ -3,5 +3,38 @@
 #include "battle_events.h"
 #include "constants/battle_events.h"
 
-IWRAM_DATA u8 gNbBattleEvents = 0;
+static u8 gNbBattleEvents;
 EWRAM_DATA u8 gBattleEvents[BATTLE_EVENTS_MAX_REGISTERABLE] = { BATTLE_EVENT_NONE };
+
+void registerBattleEvent(u8 battleEvent){
+    //reached the limit
+    if (gNbBattleEvents == BATTLE_EVENTS_MAX_REGISTERABLE) {
+        //how could i warn btw? shout in a message box?
+        return;
+    }
+    gBattleEvents[gNbBattleEvents] = battleEvent;
+    gNbBattleEvents += 1;
+}
+
+void unregisterBattlesEvents(){
+    u8 i;
+    for (i = 0; i < gNbBattleEvents; i++){
+        gBattleEvents[i] = BATTLE_EVENT_NONE;
+    }
+    gNbBattleEvents = 0;
+}
+
+void execBattleEvents(){
+    u8 i;
+    for (i = 0; i < gNbBattleEvents; i++){
+        battleEventsMegaSwitch(gBattleEvents[i]);
+    }
+}
+
+void battleEventsMegaSwitch(u8 battleEvent){
+    switch (battleEvent)
+    {
+    case BATTLE_EVENT_NONE:
+        break;
+    }
+}
