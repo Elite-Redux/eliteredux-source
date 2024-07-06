a1abdbf441a21269d98e806fe8a4ec46ce00dd79
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-07-06 17:57:36 +0200

    testing the commands I've just put

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -54,6 +54,7 @@
 #include "constants/vars.h"
 #include "constants/weather.h"
 #include "constants/quests.h"
+#include "constants/battle_events.h"
 	.include "asm/macros.inc"
 	.include "asm/macros/event.inc"
 	.include "constants/constants.inc"
