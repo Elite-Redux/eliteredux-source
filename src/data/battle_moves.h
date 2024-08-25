82e7c422e9adf22aa66ec0b4f0768753dbe991f8
tinkashadow <knolan515@gmail.com>
2024-08-24 21:38:39 -0500

    im an idiot this already worked properly

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -621,7 +621,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_SELECTED,
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_SOUND,
         .split = SPLIT_SPECIAL,
-        .argument = TYPE_ROCK,
+        .argument = TYPE_STEEL,
     },
     [MOVE_DISABLE] =
     {
