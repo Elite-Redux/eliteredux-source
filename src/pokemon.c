f28ce9399281698d248d2496702e27a15d6ff692
Mawootad <adinfinatioma@gmail.com>
2024-08-28 20:02:21 -0700

    Fix Ursaluna Bloodmoon using Ursaluna's level up learnset

--- src/pokemon.c
+++ src/pokemon.c
@@ -10812,6 +10812,7 @@ u16 getLearnsetMon(u16 species)
         case SPECIES_DEOXYS:
         case SPECIES_WORMADAM:
         case SPECIES_BURMY:
+        case SPECIES_URSALUNA_BLOODMOON:
             return species;
         
         default:
