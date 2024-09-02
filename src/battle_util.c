6fd4928810251519a72becc1097b3df1196f96ad
Mawootad <adinfinatioma@gmail.com>
2024-09-01 17:54:33 -0700

    Fix damage calc showing immune moves as doing 1 damage

--- src/battle_util.c
+++ src/battle_util.c
@@ -14766,9 +14766,6 @@ s32 DoMoveDamageCalcBattleMenu(u16 move, u8 battlerAtk, u8 battlerDef, u8* moveT
     dmg *= 100 - randomFactor;
     dmg /= 100;
 
-    if (dmg == 0)
-        dmg = 1;
-
     return dmg;
 }
 
