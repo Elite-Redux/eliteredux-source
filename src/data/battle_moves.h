9ccf48ca2af6535f91ce73cb437e3edb13002862
Mawootad <adinfinatioma@gmail.com>
2024-09-07 19:17:36 -0700

    Double hit

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -5609,13 +5609,13 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_DOUBLE_HIT] =
     {
         .effect = EFFECT_DOUBLE_HIT,
-        .power = 35,
+        .power = 45,
         .type = TYPE_NORMAL,
         .accuracy = 100,
         .pp = 10,
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
-        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_TWO_STRIKES,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_TWO_STRIKES | FLAG_HIGH_CRIT,
         .split = SPLIT_PHYSICAL,
         .parentalBondBanned = TRUE,
     },
