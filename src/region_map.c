6c0c4271b094eec57633cd152a5727716504d3f9
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-09-12 22:25:16 +0200

    added fly location indicator on fly map

--- src/region_map.c
+++ src/region_map.c
@@ -439,6 +439,17 @@ static const u16 sRedOutlineFlyDestinations[][2] =
         MAPSEC_BATTLE_FRONTIER
     },
     {
+        FLAG_VISITED_MT_CHIMNEY,
+        MAPSEC_MT_CHIMNEY
+    },
+    {   
+        FLAG_VISITED_MT_PYRE, 
+        MAPSEC_MT_PYRE
+    },
+    {
+
+    },
+    {
         -1,
         MAPSEC_NONE
     }
