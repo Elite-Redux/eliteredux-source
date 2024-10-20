fead20bcb368ef7f7a71250222efb015ce5e4c80
Mawootad <adinfinatioma@gmail.com>
2024-10-20 09:58:42 -0700

    Fix psychic noise

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -11409,6 +11409,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .pp = 15,
         .secondaryEffectChance = 100,
         .target = MOVE_TARGET_SELECTED,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_SHEER_FORCE_BOOST | FLAG_SOUND,
         .split = SPLIT_SPECIAL,
         .argument = MOVE_EFFECT_PSYCHIC_NOISE,
     },
