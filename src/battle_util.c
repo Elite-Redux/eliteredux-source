7e5e2eb8406022f4e79220ca765d66ef867cb5f4
tinkashadow <knolan515@gmail.com>
2024-10-20 23:41:46 -0500

    fixes

--- src/battle_util.c
+++ src/battle_util.c
@@ -13870,6 +13870,13 @@ int HandleDefenderAbilityAs(int ability, int battler, int attacker, int move, in
 
             UseOutOfTurnAttack(battler, attacker, ability, MOVE_ICY_WIND, 0);
             break;
+
+        case ABILITY_WILDFIRE:
+            if (!ShouldApplyOnHitAffect(attacker)) break;
+            if (!IsMoveMakingContact(move, attacker)) break;
+
+            UseOutOfTurnAttack(battler, attacker, ability, MOVE_FIRE_SPIN, 20);
+            break;
         
         case ABILITY_SNAP_TRAP_WHEN_HIT:
             if (!ShouldApplyOnHitAffect(attacker)) break;
@@ -15322,13 +15329,6 @@ int HandleSwitchInAbilityAs(int ability, int battler)
         
         case ABILITY_DREAM_WHIMSY:
             UseEntryMove(battler, ability, MOVE_YAWN, 0);
-            break;
-        
-        case ABILITY_WILDFIRE:
-            if (!ShouldApplyOnHitAffect(attacker)) break;
-            if (!IsMoveMakingContact(move, attacker)) break;
-
-            UseOutOfTurnAttack(battler, attacker, ability, MOVE_FIRE_SPIN, 20);
             break;
         
         case ABILITY_JUMP_SCARE:
