b18fb558a328b05e99178f8865a9b8aac24a9295
Mawootad <adinfinatioma@gmail.com>
2024-10-29 18:25:58 -0700

    Fix crit rates being too high

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2165,13 +2165,13 @@ s32 CalcCritChanceStage(u8 battlerAtk, u8 battlerDef, u32 move, bool32 recordAbi
                         || GET_BASE_SPECIES_ID(gBattleMons[gBattlerAttacker].species) == SPECIES_CHANSEY
                         || GET_BASE_SPECIES_ID(gBattleMons[gBattlerAttacker].species) == SPECIES_BLISSEY))
                     + BENEFITS_FROM_LEEK(battlerAtk, holdEffectAtk)
-                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_PERFECTIONIST) && gBattleMoves[move].power <= 50 && gBattleMoves[move].power > 0)
-                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_HYPER_CUTTER))
-                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_PRECISE_FIST) && IS_IRON_FIST(battlerAtk, move))
-                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_SUPER_LUCK))
-                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_HEAVEN_ASUNDER))
+                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_PERFECTIONIST) > 0 && gBattleMoves[move].power <= 50 && gBattleMoves[move].power > 0)
+                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_HYPER_CUTTER) > 0)
+                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_PRECISE_FIST) > 0 && IS_IRON_FIST(battlerAtk, move))
+                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_SUPER_LUCK) > 0)
+                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_HEAVEN_ASUNDER) > 0)
                     + 2 * (IsAbilityOnField(ABILITY_BATTLE_AURA) > 0)
-                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_WAY_OF_PRECISION) && IS_IRON_FIST(battlerAtk, move))
+                    + (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_WAY_OF_PRECISION) > 0 && IS_IRON_FIST(battlerAtk, move))
                     + gVolatileStructs[battlerAtk].critBoost
                     + (move == MOVE_VISE_GRIP);
 
