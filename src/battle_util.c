6e0d5e4d2ebe5503d267d0cb0ea14faa025f891f
Mawootad <adinfinatioma@gmail.com>
2024-11-26 23:32:04 -0800

    Make ignore stat changes flag only negate positive boosts

--- src/battle_util.c
+++ src/battle_util.c
@@ -10221,9 +10221,9 @@ void SetSwapDamageCategory(int battler, int target, int move)
         
         case USE_HIGHEST_OFFENSE:
             {
-                int isUnaware = IsUnaware(target);
-                int atk = CalculateStat(battler, STAT_ATK, 0, move, TRUE, FALSE, isUnaware, FALSE);
-                int spAtk = CalculateStat(battler, STAT_SPATK, 0, move, TRUE, FALSE, isUnaware, FALSE);
+                int isTargetUnaware = IsUnaware(target);
+                int atk = CalculateStat(battler, STAT_ATK, 0, move, TRUE, FALSE, isTargetUnaware, FALSE);
+                int spAtk = CalculateStat(battler, STAT_SPATK, 0, move, TRUE, FALSE, isTargetUnaware, FALSE);
                 if (atk > spAtk) gSwapDamageCategory = gBattleMoves[move].split == SPLIT_SPECIAL;
                 else if (atk < spAtk) gSwapDamageCategory = gBattleMoves[move].split == SPLIT_PHYSICAL;
                 else gSwapDamageCategory = Random() % 2;
@@ -10268,8 +10268,10 @@ static u32 CalcDefenseStat(u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, 
 {
     u8 defStatToUse = 0;
     u32 defStat;
-    u8 noPositiveStatStages = isCrit || (gBattleMons[battlerDef].status2 & STATUS2_WRAPPED && BattlerHasAbility(battlerAtk, ABILITY_GRIP_PINCER, TRUE));
-    u8 isUnaware = IsUnaware(battlerAtk) || gBattleMoves[move].flags & FLAG_STAT_STAGES_IGNORED;
+    u8 noPositiveStatStages = isCrit
+        || gBattleMoves[move].flags & FLAG_STAT_STAGES_IGNORED
+        || (gBattleMons[battlerDef].status2 & STATUS2_WRAPPED && BattlerHasAbility(battlerAtk, ABILITY_GRIP_PINCER, FALSE));
+    u8 isUnaware = IsUnaware(battlerAtk);
     u16 modifier;
     int i;
 
