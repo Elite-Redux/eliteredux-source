4bafc611aa64d9918326c1dfe676a0cb9bdc1149
Mawootad <adinfinatioma@gmail.com>
2024-12-07 13:54:04 -0800

    Fix missing crit flag on devious shot

--- src/battle_message.c
+++ src/battle_message.c
@@ -915,6 +915,7 @@ static const u8 sText_RudeAwakening[] = _("{B_ATK_NAME_WITH_PREFIX} wakes up\nen
 static const u8 sText_Cutthroat[] = _("{B_ATK_NAME_WITH_PREFIX} readies\ntheir blade!");
 static const u8 sText_ItemStatProtected[] = _("{B_SCR_ACTIVE_NAME_WITH_PREFIX}'s {B_LAST_ITEM}\nprevents its stats from lowering!");
 static const u8 sText_StackChangedTypeTo[] = _("{B_STACK_1_NAME_WITH_PREFIX}'s type\nchanged to {B_BUFF1}!");
+static const u8 sText_StackRegainedHealth[] = _("{B_STACK_1_NAME_WITH_PREFIX} regained\nhealth!");
 const u8 gText_PkmnsXPreventsSwitching[] = _("{STR_VAR_1}'s Ability\nprevents switching!{PAUSE_UNTIL_PRESS}");
 static const u8 sText_PlayerDefeatedLinkTrainer[] = _("Player defeated\n{B_LINK_OPPONENT1_NAME}!");
 static const u8 sText_TwoLinkTrainersDefeated[] = _("Player beat {B_LINK_OPPONENT1_NAME}\nand {B_LINK_OPPONENT2_NAME}!");
@@ -1073,7 +1074,6 @@ static const u8 sText_Your2[] = _("your");
 static const u8 sText_Opposing2[] = _("the opposing");
 const u8 gText_NoMoreRoomForPokemon[] = _("There's no more room for Pokémon!\pThe Pokémon Boxes are full and\ncan't accept any more!");
 const u8 gText_NicknameThisPokemon[] = _("Do you want to give a nickname to\nthis {STR_VAR_1}?");
-static const u8 sText_StackRegainedHealth[] = _("{B_STACK_1_NAME_WITH_PREFIX} regained\nhealth!");
 
 const u8 *const gBattleStringsTable[BATTLESTRINGS_COUNT] =
 {
