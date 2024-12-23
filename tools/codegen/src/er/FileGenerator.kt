89eae9ceab4512894affb897514ee1fa68d98a3d
Mawootad <adinfinatioma@gmail.com>
2024-12-22 20:38:22 -0800

    Automatically create directories

--- tools/codegen/src/er/FileGenerator.kt
+++ tools/codegen/src/er/FileGenerator.kt
@@ -1,14 +1,20 @@
 package er
 
 import er.abilities.AbilitiesEnumGenerator
+import java.io.File
 
 object FileGenerator {
     @JvmStatic
     fun main(args: Array<String>) {
         val (type, file) = args
-        when (type) {
-            "abilities" -> AbilitiesEnumGenerator.generate(file)
-            else -> error("Invalid file type ${type}")
+        try {
+            File(file).parentFile.mkdirs()
+            when (type) {
+                "abilities" -> AbilitiesEnumGenerator.generate(file)
+                else -> error("Invalid file type ${type}")
+            }
+        } catch (e: Exception) {
+            throw Exception("Failed processing $type generating file $file", e)
         }
     }
 }
