0aa40980957fc48b3e381fb587bcc1c64b64c1df
tinkashadow <knolan515@gmail.com>
2025-01-09 23:25:00 -0600

    movepool additions, wood hammer super slammer boost, mr mime trickster

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -5616,6 +5616,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_SELECTED,
         .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_RECKLESS_BOOST,
         .split = SPLIT_PHYSICAL,
+        .hammerBased = TRUE,
     },
     [MOVE_AQUA_JET] =
     {
