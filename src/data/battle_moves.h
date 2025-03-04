f7e64f9c7e008f29d02be67f371c24e52c567319
tinkashadow <knolan515@gmail.com>
2025-03-04 16:31:32 -0600

    made skulberus boring

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -8142,6 +8142,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_FOES_AND_ALLY,
         .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
         .split = SPLIT_PHYSICAL,
+        .hammerBased = TRUE,
     },
     [MOVE_AURORA_VEIL] =
     {
