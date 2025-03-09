592b52d68dd3e386462b0853458adb393a54e8d7
Mawootad <adinfinatioma@gmail.com>
2025-03-08 16:49:33 -0800

    Fix surge surfer checking for weather instead of terrain

--- src/abilities.cc
+++ src/abilities.cc
@@ -2699,7 +2699,9 @@ static const Ability SurgeSurfer = {
                      "gets a 1.5x Speed boost."),
     .onStat =
         +[](ON_STAT) {
-            if (statId == STAT_SPEED && IsWeatherActive(STATUS_FIELD_ELECTRIC_TERRAIN)) *stat *= 1.5;
+            MGBA_PRINT_VALUES(*stat)
+            if (statId == STAT_SPEED && IsTerrainActive(STATUS_FIELD_ELECTRIC_TERRAIN)) *stat *= 1.5;
+            MGBA_PRINT_VALUES(*stat)
         },
 };
 
