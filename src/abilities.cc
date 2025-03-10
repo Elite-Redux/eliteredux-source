cf0ba90c0c5e9314ed56b76cd73aaaabf879db79
Mawootad <adinfinatioma@gmail.com>
2025-03-09 20:05:36 -0700

    Missing semicolon

--- src/abilities.cc
+++ src/abilities.cc
@@ -9697,14 +9697,13 @@ static const Ability BlightScale = {
     .randomizerBanned = TRUE,
 };
 
-static const Ability Gunman =
-    {
-        .name = $("Gunman"),
-        .description = $("Mega Launcher + All attacks are\n"
-                         "Mega Launcher boosted."),
-        .onOffensiveMultiplier = MegaLauncher.onOffensiveMultiplier,
-        .megaLauncherBoost = TRUE,
-}
+static const Ability Gunman = {
+    .name = $("Gunman"),
+    .description = $("Mega Launcher + All attacks are\n"
+                     "Mega Launcher boosted."),
+    .onOffensiveMultiplier = MegaLauncher.onOffensiveMultiplier,
+    .megaLauncherBoost = TRUE,
+};
 
 static const Ability iamsteve = {
     .name = $("i am steve"),
