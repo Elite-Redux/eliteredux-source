d9de81705f395720937c029b273016f96fe61fd4
Mawootad <adinfinatioma@gmail.com>
2025-04-04 23:06:08 -0700

    Update randomizer banned flags

--- tools/codegen/src/er/data/RandomizerBannedGenerator.kt
+++ tools/codegen/src/er/data/RandomizerBannedGenerator.kt
@@ -3,19 +3,28 @@ package er.data
 import er.FileGenerator.IND
 import er.Generator
 import er.GeneratorUtils.SPECIES_LIST
-import er.proto.Species.RandomizeBanned.BANNED_NONE
-import er.proto.Species.RandomizeBanned.LEGENDARY
+import er.proto.Species
+import er.proto.Species.RandomizeBanned.*
 import java.io.OutputStreamWriter
 
 object RandomizerBannedGenerator : Generator {
+    private fun randomizerCondition(species: Species) = when {
+        species.randomizerBanned == LEGENDARY -> "allowLegendaries"
+        species.randomizerBanned == PARADOX -> "allowParadox"
+        species.randomizerBanned != BANNED_NONE -> "FALSE"
+        species.megaList.isNotEmpty() -> "FALSE"
+        species.primalList.isNotEmpty() -> "FALSE"
+        species.hasFormShiftOf() -> "FALSE"
+        else -> "TRUE"
+    }
+
     override fun generate(writer: OutputStreamWriter) {
         writer.appendLine(
             """
-            |int isSpeciesRandomizerBanned(SpeciesEnum species, int allowLegendaries) {
+            |int isSpeciesRandomizerAllowed(SpeciesEnum species, int allowLegendaries, int allowParadox) {
             |${IND}switch (species) {
             |$IND$IND${
-                SPECIES_LIST.filter { it.randomizerBanned != BANNED_NONE }
-                    .joinToString("\n$IND$IND") { "case ${it.id}: return ${if (it.randomizerBanned == LEGENDARY) "allowLegendaries" else "TRUE"};" }
+                SPECIES_LIST.joinToString("\n$IND$IND") { "case ${it.id}: return ${randomizerCondition(it)};" }
             }
             |$IND${IND}default: return FALSE;
             |$IND}
