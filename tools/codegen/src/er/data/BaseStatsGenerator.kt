1dba1b859bc8e27733f4006382f171c75e740c65
LuckyCapybara <215329213+LuckyCapybara@users.noreply.github.com>
2025-09-19 13:34:22 -0600

    Update generator to include tag team flag

--- tools/codegen/src/er/data/BaseStatsGenerator.kt
+++ tools/codegen/src/er/data/BaseStatsGenerator.kt
@@ -64,6 +64,7 @@ object BaseStatsGenerator : Generator {
             val flags = buildList {
                 if (species.heads == 2) add ("F_TWO_HEADED")
                 if (species.heads == 3) add ("F_THREE_HEADED")
+                if (species.isTagTeam) add ("F_TAG_TEAM")
             }.joinToString(" | ")
 
             if (flags.isNotEmpty()) lines += ".flags = $flags"
