d0ad4529a96f3f5bd1118d55512931cae2a6d9d0
BelialClover <67656448+BelialClover@users.noreply.github.com>
2025-11-11 21:13:55 -0600

    Sunhollow big updates

--- src/debug.c
+++ src/debug.c
@@ -440,7 +440,7 @@ static const u8 sDebugText_AccessPC[] = _("Access PC…{CLEAR_TO 110}{RIGHT_ARRO
 static const u8 sDebugText_Fill_Box[] = _("Fill Box…{CLEAR_TO 110}{RIGHT_ARROW}");
 static const u8 sDebugText_Cancel[] = _("Cancel");
 // Script menu
-static const u8 sDebugText_Util_Script_1[] = _("Script 1");
+static const u8 sDebugText_Util_Script_1[] = _("Story Jump");
 static const u8 sDebugText_Util_Script_2[] = _("Script 2");
 static const u8 sDebugText_Util_Script_3[] = _("Script 3");
 static const u8 sDebugText_Util_Script_4[] = _("Mega Bracelet");
