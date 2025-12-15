3dfa13093717fbedef2fdbc64b86c7df9ebcc4bc
iunrr <157452178+iunrr@users.noreply.github.com>
2025-12-15 06:51:36 -0800

    Replace generic stat raise string in BattleScript_StackBattlerStatUp with specific stat raise message

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -9555,7 +9555,7 @@ BattleScript_StackBattlerStatUp::
 	jumpifbyte CMP_EQUAL, cMULTISTRING_CHOOSER, B_MSG_STAT_WONT_INCREASE, BattleScript_RestoreAttackerReturn
 	setgraphicalstatchangevalues
 	playanimation BS_ABILITY_BATTLER, B_ANIM_STATS_CHANGE, sB_ANIM_ARG1
-	printstring STRINGID_ATTACKER_STATS_ROSE
+	printfromtable gStatUpStringIds
 	readattackerfromstack3
 	return
 
