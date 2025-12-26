0eb748563592a7cfec37078f14b6d0a63697616c
iunrr <157452178+iunrr@users.noreply.github.com>
2025-12-25 18:04:29 -0800

    Remove leftover contact check from ability Freezing Point

--- src/abilities.cc
+++ src/abilities.cc
@@ -6138,7 +6138,6 @@ constexpr Ability Impl<ABILITY_AFTERSHOCK> = {
 ON_EITHER(FreezingPoint) {
     CHECK(ShouldApplyOnHitEffect(opponent))
     CHECK(CanGetFrostbite(opponent))
-    CHECK(IsMoveMakingContact(move, gBattlerAttacker))
     int chance = IsMoveMakingContact(move, gBattlerAttacker) ? 20 : 30;
     CHECK(Random() % 100 < chance)
 
