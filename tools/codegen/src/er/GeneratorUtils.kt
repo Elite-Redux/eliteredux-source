9f92d86e9f2d91b385620fa3e3798b9434107dcf
Mawootad <adinfinatioma@gmail.com>
2026-03-01 09:30:37 -0800

    Fix INCONSISTENT_GENDER_MONS not working for megas/primals

--- tools/codegen/src/er/GeneratorUtils.kt
+++ tools/codegen/src/er/GeneratorUtils.kt
@@ -17,175 +17,208 @@ import java.io.OutputStreamWriter
 
 object GeneratorUtils {
 
-    val TRAINERS_LIST by lazy {
-        TextFormat.parse(File("../../proto/TrainerList.textproto").readText(), TrainerList::class.java).trainerList
-    }
+  val TRAINERS_LIST by lazy {
+    TextFormat.parse(File("../../proto/TrainerList.textproto").readText(), TrainerList::class.java)
+      .trainerList
+  }
 
-    val ITEMS_LIST by lazy {
-        (Pocket.entries.filter { it != Pocket.POCKET_NONE && it != Pocket.UNRECOGNIZED }.map {
-            it.name.removePrefix("POCKET_").split("_")
-                .joinToString("") { s -> s.lowercase().replaceFirstChar { c -> c.uppercaseChar() } }
-        } + "Unused").flatMap {
-            if (File("../../proto/items/${it}List.textproto").exists()) {
-                TextFormat.parse(
-                    File("../../proto/items/${it}List.textproto").readText(),
-                    ItemList::class.java
-                ).itemList
-            } else {
-                emptyList()
-            }
-        }.filter { it.name.isNotEmpty() && it.image.isNotEmpty() && it.palette.isNotEmpty() }
-    }
-
-    val ABILITIES_LIST by lazy {
-        TextFormat.parse(File("../../proto/AbilityList.textproto").readText(), AbilityList::class.java).abilityList
-    }
-
-    val MOVES_LIST by lazy {
-        TextFormat.parse(File("../../proto/MoveList.textproto").readText(), MoveList::class.java).movesList
-    }
-
-    val FULL_SPECIES_LIST by lazy {
-        TextFormat.parse(
-            File("../../proto/SpeciesList.textproto").readText(),
-            SpeciesList::class.java
-        ).speciesList
-    }
-
-    val SPECIES_LIST by lazy {
-        FULL_SPECIES_LIST.filter { it.randomizerBanned != SPECIES_HIDDEN }
-    }
-
-    val NO_EGG_LIST by lazy {
-        SPECIES_LIST.filter { it.id != SpeciesEnum.SPECIES_EGG }
-    }
-
-    val SPECIES_COUNT by lazy {
-        SPECIES_LIST.maxOf { it.id.number } + 1
-    }
-
-    val REAL_SPECIES_COUNT by lazy {
-        NO_EGG_LIST.maxOf { it.id.number } + 1
-    }
-
-    val SPECIES_MAP by lazy {
-        SPECIES_LIST.associateBy { it.id }
-    }
-
-    val RANDOMIZER_CONFIG by lazy {
-        TextFormat.parse(
-            File("../../proto/LimitedRandomizerConfig.textproto").readText(),
-            LimitedRandomizerConfig::class.java
-        )
-    }
-
-    fun findLearnsetForSpecies(species: Species, learnsetChain: MutableList<SpeciesEnum> = mutableListOf()): Learnset {
-        val id = species.id
-
-        check(id !in learnsetChain) { "Self referential moveset found: ${learnsetChain.joinToString(" -> ")} -> $id" }
-        learnsetChain += id
-
-        return when {
-            species.id == SpeciesEnum.SPECIES_NONE -> species.learnset
-            species.megaList.isNotEmpty() -> findLearnsetForSpecies(SPECIES_MAP[species.megaList.first().from]!!, learnsetChain)
-            species.primalList.isNotEmpty() -> findLearnsetForSpecies(SPECIES_MAP[species.primalList.first().from]!!, learnsetChain)
-            species.hasBattleForm() -> findLearnsetForSpecies(SPECIES_MAP[species.battleForm.of]!!, learnsetChain)
-            species.hasLearnset() -> species.learnset
-            species.usesLearnset != SpeciesEnum.SPECIES_NONE -> findLearnsetForSpecies(SPECIES_MAP[species.usesLearnset]!!, learnsetChain)
-            species.formShiftOf != SpeciesEnum.SPECIES_NONE -> findLearnsetForSpecies(SPECIES_MAP[species.formShiftOf]!!, learnsetChain)
-            else -> findLearnsetForSpecies(SPECIES_MAP[species.formOf]!!, learnsetChain)
+  val ITEMS_LIST by lazy {
+    (Pocket.entries
+        .filter { it != Pocket.POCKET_NONE && it != Pocket.UNRECOGNIZED }
+        .map {
+          it.name.removePrefix("POCKET_").split("_").joinToString("") { s ->
+            s.lowercase().replaceFirstChar { c -> c.uppercaseChar() }
+          }
+        } + "Unused")
+      .flatMap {
+        if (File("../../proto/items/${it}List.textproto").exists()) {
+          TextFormat.parse(
+              File("../../proto/items/${it}List.textproto").readText(),
+              ItemList::class.java,
+            )
+            .itemList
+        } else {
+          emptyList()
         }
+      }
+      .filter { it.name.isNotEmpty() && it.image.isNotEmpty() && it.palette.isNotEmpty() }
+  }
+
+  val ABILITIES_LIST by lazy {
+    TextFormat.parse(File("../../proto/AbilityList.textproto").readText(), AbilityList::class.java)
+      .abilityList
+  }
+
+  val MOVES_LIST by lazy {
+    TextFormat.parse(File("../../proto/MoveList.textproto").readText(), MoveList::class.java)
+      .movesList
+  }
+
+  val FULL_SPECIES_LIST by lazy {
+    TextFormat.parse(File("../../proto/SpeciesList.textproto").readText(), SpeciesList::class.java)
+      .speciesList
+  }
+
+  val SPECIES_LIST by lazy { FULL_SPECIES_LIST.filter { it.randomizerBanned != SPECIES_HIDDEN } }
+
+  val NO_EGG_LIST by lazy { SPECIES_LIST.filter { it.id != SpeciesEnum.SPECIES_EGG } }
+
+  val SPECIES_COUNT by lazy { SPECIES_LIST.maxOf { it.id.number } + 1 }
+
+  val REAL_SPECIES_COUNT by lazy { NO_EGG_LIST.maxOf { it.id.number } + 1 }
+
+  val SPECIES_MAP by lazy { SPECIES_LIST.associateBy { it.id } }
+
+  val SpeciesEnum.data: Species
+    get() = SPECIES_MAP[this]!!
+
+  val RANDOMIZER_CONFIG by lazy {
+    TextFormat.parse(
+      File("../../proto/LimitedRandomizerConfig.textproto").readText(),
+      LimitedRandomizerConfig::class.java,
+    )
+  }
+
+  fun findLearnsetForSpecies(
+    species: Species,
+    learnsetChain: MutableList<SpeciesEnum> = mutableListOf(),
+  ): Learnset {
+    val id = species.id
+
+    check(id !in learnsetChain) {
+      "Self referential moveset found: ${learnsetChain.joinToString(" -> ")} -> $id"
     }
+    learnsetChain += id
 
-    fun expandLearnset(learnset: Learnset, species: Species) =
-        UNIVERSAL_TUTORS + UNIVERSAL_ATTACKS.takeIf { learnset.universalTutors != UniversalTutors.NO_ATTACKS }
-            .orEmpty() + UNIVERSAL_GENDERED.takeIf { !species.genderless }.orEmpty() + learnset.tutorList
-
-    private val UNIVERSAL_TUTORS = MOVES_LIST.filter { it.tutor == TutorType.TUTOR_UNIVERSAL_STATUS }.map { it.id }
-    private val UNIVERSAL_ATTACKS = MOVES_LIST.filter { it.tutor == TutorType.TUTOR_UNIVERSAL_ATTACK }.map { it.id }
-    private val UNIVERSAL_GENDERED =
-        MOVES_LIST.filter { it.tutor == TutorType.TUTOR_UNIVERSAL_STATUS_GENDERED }.map { it.id }
-
-    fun Species.resolveVisuals(): Visuals =
-        if (hasReuseVisuals()) SPECIES_MAP[reuseVisuals]!!.resolveVisuals() else this.visuals
-
-    fun <T> ProtocolMessageEnum.getOption(extension: GeneratedExtension<EnumValueOptions, T>): T =
-        valueDescriptor.getOption(extension)
-
-    fun <T> EnumValueDescriptor.getOption(extension: GeneratedExtension<EnumValueOptions, T>): T =
-        options.getExtension(extension)
-
-    fun <T> FieldDescriptor.getOption(extension: GeneratedExtension<FieldOptions, T>): T =
-        toProto().options.getExtension(extension)
-
-    fun String.fileID() =
-        lowercase().replace("[^a-z0-9_]".toRegex()) { "E${it.value.replace("\\", "/").single().code}" }
-
-    /**
-     * Takes a list of key-value pairs and creates a set of mappings of keys/values to a shared index value.
-     */
-    fun <Key, Value> List<Pair<Key, Value>>.createDedupMaps(): Pair<Map<Key, Int>, Map<Value, Int>> {
-        val groups = groupBy({ it.first }, { it.second })
-
-        val keyIds = groups.keys.withIndex().associate { it.value to it.index }
-        val valueIds =
-            groups.flatMap { (key, values) -> values.map { it to keyIds[key]!! } }
-                .toMap()
-        return keyIds to valueIds
+    return when {
+      species.id == SpeciesEnum.SPECIES_NONE -> species.learnset
+      species.megaList.isNotEmpty() ->
+        findLearnsetForSpecies(SPECIES_MAP[species.megaList.first().from]!!, learnsetChain)
+      species.primalList.isNotEmpty() ->
+        findLearnsetForSpecies(SPECIES_MAP[species.primalList.first().from]!!, learnsetChain)
+      species.hasBattleForm() ->
+        findLearnsetForSpecies(SPECIES_MAP[species.battleForm.of]!!, learnsetChain)
+      species.hasLearnset() -> species.learnset
+      species.usesLearnset != SpeciesEnum.SPECIES_NONE ->
+        findLearnsetForSpecies(SPECIES_MAP[species.usesLearnset]!!, learnsetChain)
+      species.formShiftOf != SpeciesEnum.SPECIES_NONE ->
+        findLearnsetForSpecies(SPECIES_MAP[species.formShiftOf]!!, learnsetChain)
+      else -> findLearnsetForSpecies(SPECIES_MAP[species.formOf]!!, learnsetChain)
     }
+  }
 
-    fun Map<*, Int>.printLookupTable(tableSignature: String, prefix: String, writer: OutputStreamWriter) {
-        writer.appendLine(
-            """
+  fun expandLearnset(learnset: Learnset, species: Species) =
+    UNIVERSAL_TUTORS +
+      UNIVERSAL_ATTACKS.takeIf { learnset.universalTutors != UniversalTutors.NO_ATTACKS }
+        .orEmpty() +
+      UNIVERSAL_GENDERED.takeIf { !species.genderless }.orEmpty() +
+      learnset.tutorList
+
+  private val UNIVERSAL_TUTORS =
+    MOVES_LIST.filter { it.tutor == TutorType.TUTOR_UNIVERSAL_STATUS }.map { it.id }
+  private val UNIVERSAL_ATTACKS =
+    MOVES_LIST.filter { it.tutor == TutorType.TUTOR_UNIVERSAL_ATTACK }.map { it.id }
+  private val UNIVERSAL_GENDERED =
+    MOVES_LIST.filter { it.tutor == TutorType.TUTOR_UNIVERSAL_STATUS_GENDERED }.map { it.id }
+
+  fun Species.resolveVisuals(): Visuals =
+    if (hasReuseVisuals()) SPECIES_MAP[reuseVisuals]!!.resolveVisuals() else this.visuals
+
+  fun <T> ProtocolMessageEnum.getOption(extension: GeneratedExtension<EnumValueOptions, T>): T =
+    valueDescriptor.getOption(extension)
+
+  fun <T> EnumValueDescriptor.getOption(extension: GeneratedExtension<EnumValueOptions, T>): T =
+    options.getExtension(extension)
+
+  fun <T> FieldDescriptor.getOption(extension: GeneratedExtension<FieldOptions, T>): T =
+    toProto().options.getExtension(extension)
+
+  fun String.fileID() =
+    lowercase().replace("[^a-z0-9_]".toRegex()) { "E${it.value.replace("\\", "/").single().code}" }
+
+  /**
+   * Takes a list of key-value pairs and creates a set of mappings of keys/values to a shared index
+   * value.
+   */
+  fun <Key, Value> List<Pair<Key, Value>>.createDedupMaps(): Pair<Map<Key, Int>, Map<Value, Int>> {
+    val groups = groupBy({ it.first }, { it.second })
+
+    val keyIds = groups.keys.withIndex().associate { it.value to it.index }
+    val valueIds = groups.flatMap { (key, values) -> values.map { it to keyIds[key]!! } }.toMap()
+    return keyIds to valueIds
+  }
+
+  fun Map<*, Int>.printLookupTable(
+    tableSignature: String,
+    prefix: String,
+    writer: OutputStreamWriter,
+  ) {
+    writer.appendLine(
+      """
             |$tableSignature = {
             |$IND${entries.joinToString("\n$IND") { "[${it.key}] = $prefix${it.value}," }}
             |};
-            |""".trimMargin()
-        )
-    }
-
-    enum class PrintMode { FILE, STRING }
-
-    /**
-     * Lookup table definition for deduping
-     *
-     * @param signature signature for the table to print, will have const prepended and '= {' appended
-     * @param data list of keys to string values
-     * @param entryFunction transforms a key plus dedup entry name to a table entry. Has indentation prepended and ',' appended.
-     */
-    data class LookupTable<T>(
-        val signature: String,
-        val data: List<Pair<T, String?>>,
-        val entryFunction: (T, String) -> String = { key, name -> "[$key] = $name" }
+            |"""
+        .trimMargin()
     )
+  }
 
-    /**
-     * Prints a set of lookup tables to string values with deduplicated strings.
-     *
-     * @param mode whether the string value is a filepath or a display string
-     * @param prefix prefix of the static dedup entries
-     * @param valueFunction method that generates the dedup entry, (field, string) -> code. Will be prepended with static const and appended with ;.
-     * @param tables table entries to include. Entries included across tables will be deduped.
-     */
-    fun <T> printLookupTables(
-        writer: OutputStreamWriter,
-        mode: PrintMode,
-        prefix: String,
-        valueFunction: (String, String) -> String,
-        vararg tables: LookupTable<T>
-    ) {
-        fun String.dedupEntry() = prefix + if (mode == PrintMode.FILE) fileID() else TODO("String mode not implemented")
+  enum class PrintMode {
+    FILE,
+    STRING,
+  }
 
-        tables.asSequence().flatMap { it.data }.map { it.second }.filterNotNull().filter { it.isNotBlank() }
-            .associateBy { it.dedupEntry() }.entries.forEach {
-                writer.appendLine("static const ${valueFunction(it.key, it.value)};")
-            }
+  /**
+   * Lookup table definition for deduping
+   *
+   * @param signature signature for the table to print, will have const prepended and '= {' appended
+   * @param data list of keys to string values
+   * @param entryFunction transforms a key plus dedup entry name to a table entry. Has indentation
+   *   prepended and ',' appended.
+   */
+  data class LookupTable<T>(
+    val signature: String,
+    val data: List<Pair<T, String?>>,
+    val entryFunction: (T, String) -> String = { key, name -> "[$key] = $name" },
+  )
 
-        for (table in tables) {
-            writer.appendLine("const ${table.signature} = {")
-            table.data.filter { !it.second.isNullOrBlank() }
-                .forEach { writer.appendLine("$IND${table.entryFunction(it.first, it.second!!.dedupEntry())},") }
-            writer.appendLine("};\n")
+  /**
+   * Prints a set of lookup tables to string values with deduplicated strings.
+   *
+   * @param mode whether the string value is a filepath or a display string
+   * @param prefix prefix of the static dedup entries
+   * @param valueFunction method that generates the dedup entry, (field, string) -> code. Will be
+   *   prepended with static const and appended with ;.
+   * @param tables table entries to include. Entries included across tables will be deduped.
+   */
+  fun <T> printLookupTables(
+    writer: OutputStreamWriter,
+    mode: PrintMode,
+    prefix: String,
+    valueFunction: (String, String) -> String,
+    vararg tables: LookupTable<T>,
+  ) {
+    fun String.dedupEntry() =
+      prefix + if (mode == PrintMode.FILE) fileID() else TODO("String mode not implemented")
+
+    tables
+      .asSequence()
+      .flatMap { it.data }
+      .map { it.second }
+      .filterNotNull()
+      .filter { it.isNotBlank() }
+      .associateBy { it.dedupEntry() }
+      .entries
+      .forEach { writer.appendLine("static const ${valueFunction(it.key, it.value)};") }
+
+    for (table in tables) {
+      writer.appendLine("const ${table.signature} = {")
+      table.data
+        .filter { !it.second.isNullOrBlank() }
+        .forEach {
+          writer.appendLine("$IND${table.entryFunction(it.first, it.second!!.dedupEntry())},")
         }
+      writer.appendLine("};\n")
     }
+  }
 }
--- tools/codegen/src/er/data/EvolutionsGenerator.kt
+++ tools/codegen/src/er/data/EvolutionsGenerator.kt
@@ -7,6 +7,7 @@ import er.GeneratorUtils.REAL_SPECIES_COUNT
 import er.GeneratorUtils.SPECIES_LIST
 import er.GeneratorUtils.SPECIES_MAP
 import er.GeneratorUtils.createDedupMaps
+import er.GeneratorUtils.data
 import er.GeneratorUtils.printLookupTable
 import er.proto.Species
 import er.proto.Species.Gender.GENDER_NONE
@@ -14,142 +15,168 @@ import er.proto.SpeciesEnum
 import java.io.OutputStreamWriter
 
 object EvolutionsGenerator : Generator {
-    private const val EVO_PREFIX = "__sEvoList_"
-    private const val FORM_PREFX = "__sFormList_"
+  private const val EVO_PREFIX = "__sEvoList_"
+  private const val FORM_PREFX = "__sFormList_"
 
-    private val INCONSISTENT_GENDER_MONS =
-        setOf(
-            SpeciesEnum.SPECIES_SHEDINJA,
-            SpeciesEnum.SPECIES_MARILL,
-            SpeciesEnum.SPECIES_MEOWSTIC_MEGA,
-        )
+  private val INCONSISTENT_GENDER_MONS =
+    setOf(
+      SpeciesEnum.SPECIES_SHEDINJA,
+      SpeciesEnum.SPECIES_MARILL,
+      SpeciesEnum.SPECIES_MEOWSTIC_MEGA,
+    )
 
-    private data class Evo(val method: EvoOrFormType, val condition: String, val to: SpeciesEnum) {
-        override fun toString() = "{$method, $condition, $to}"
+  private data class Evo(val method: EvoOrFormType, val condition: String, val to: SpeciesEnum) {
+    override fun toString() = "{$method, $condition, $to}"
+  }
+
+  private enum class EvoOrFormType {
+    EVO_FORM_SHIFT,
+    EVO_FORM_SHIFT_GENDER,
+    EVO_MEGA_EVOLUTION,
+    EVO_PRIMAL_REVERSION,
+    EVO_MOVE_MEGA_EVOLUTION,
+    EVO_LEVEL_MALE,
+    EVO_LEVEL_FEMALE,
+    EVO_LEVEL_SHEDINJA,
+    EVO_LEVEL_NINJASK,
+    EVO_LEVEL,
+    EVO_DEEVOLUTION,
+    EVO_UNMEGA,
+  }
+
+  override fun generate(writer: OutputStreamWriter) {
+
+    val allEvoPairs =
+      (SPECIES_LIST.flatMap {
+          it.evoList.filter { evo -> evo.gender == GENDER_NONE }.map { evo -> it.id to evo.to }
+        } +
+          SPECIES_LIST.flatMap { it.megaList.map { mega -> mega.from to it.id } } +
+          SPECIES_LIST.flatMap { it.primalList.map { primal -> primal.from to it.id } })
+        .filter { it.second !in INCONSISTENT_GENDER_MONS }
+
+    val inconsistentGenderEvos =
+      allEvoPairs.filter {
+        it.first.data.genderless != it.second.data.genderless ||
+          it.first.data.percentFemale != it.second.data.percentFemale
+      }
+
+    check(inconsistentGenderEvos.isEmpty()) {
+      "Gender ratios don't match in evos, is this intentional?: $inconsistentGenderEvos"
     }
 
-    private enum class EvoOrFormType {
-        EVO_FORM_SHIFT,
-        EVO_FORM_SHIFT_GENDER,
-        EVO_MEGA_EVOLUTION,
-        EVO_PRIMAL_REVERSION,
-        EVO_MOVE_MEGA_EVOLUTION,
-        EVO_LEVEL_MALE,
-        EVO_LEVEL_FEMALE,
-        EVO_LEVEL_SHEDINJA,
-        EVO_LEVEL_NINJASK,
-        EVO_LEVEL,
-        EVO_DEEVOLUTION,
-        EVO_UNMEGA,
-    }
+    val deevos =
+      SPECIES_LIST.filter { it.allowDeevolutionTo }
+        .flatMap { species -> species.evoList.map { it.to to species.id } }
+        .toMap()
+        .mapValues { Evo(EvoOrFormType.EVO_DEEVOLUTION, "TRUE", it.value) }
 
-    override fun generate(writer: OutputStreamWriter) {
-
-        val allEvoPairs = SPECIES_LIST.flatMap {
-            it.evoList.filter { evo -> evo.gender == GENDER_NONE && evo.to !in INCONSISTENT_GENDER_MONS }
-                .map { evo -> it.id to evo.to }
-        } + SPECIES_LIST.flatMap { it.megaList.map { mega -> it.id to mega.from } } + SPECIES_LIST.flatMap { it.primalList.map { primal -> it.id to primal.from } }
-
-        val inconsistentGenderEvos =
-            allEvoPairs.filter { SPECIES_MAP[it.first]!!.genderless != SPECIES_MAP[it.second]!!.genderless || SPECIES_MAP[it.first]!!.percentFemale != SPECIES_MAP[it.second]!!.percentFemale }
-
-        check(inconsistentGenderEvos.isEmpty()) { "Gender ratios don't match in evos, is this intentional?: $inconsistentGenderEvos" }
-
-        val deevos = SPECIES_LIST.filter { it.allowDeevolutionTo }
-            .flatMap { species -> species.evoList.map { it.to to species.id } }.toMap()
-            .mapValues { Evo(EvoOrFormType.EVO_DEEVOLUTION, "TRUE", it.value) }
-
-        // De-evolutions are expected to come first
-        val (evoIds, speciesEvoIds) = NO_EGG_LIST.map { species ->
-            listOfNotNull(deevos[species.id]) + species.evoList.filter { it.to in SPECIES_MAP }.map {
+    // De-evolutions are expected to come first
+    val (evoIds, speciesEvoIds) =
+      NO_EGG_LIST.map { species ->
+          listOfNotNull(deevos[species.id]) +
+            species.evoList
+              .filter { it.to in SPECIES_MAP }
+              .map {
                 Evo(
-                    when {
-                        it.gender == Species.Gender.MALE -> EvoOrFormType.EVO_LEVEL_MALE
-                        it.gender == Species.Gender.FEMALE -> EvoOrFormType.EVO_LEVEL_FEMALE
-                        species.id == SpeciesEnum.SPECIES_SHEDINJA -> EvoOrFormType.EVO_LEVEL_SHEDINJA
-                        species.id == SpeciesEnum.SPECIES_NINJASK -> EvoOrFormType.EVO_LEVEL_NINJASK
-                        else -> EvoOrFormType.EVO_LEVEL
-                    }, it.level.toString(), it.to
+                  when {
+                    it.gender == Species.Gender.MALE -> EvoOrFormType.EVO_LEVEL_MALE
+                    it.gender == Species.Gender.FEMALE -> EvoOrFormType.EVO_LEVEL_FEMALE
+                    species.id == SpeciesEnum.SPECIES_SHEDINJA -> EvoOrFormType.EVO_LEVEL_SHEDINJA
+                    species.id == SpeciesEnum.SPECIES_NINJASK -> EvoOrFormType.EVO_LEVEL_NINJASK
+                    else -> EvoOrFormType.EVO_LEVEL
+                  },
+                  it.level.toString(),
+                  it.to,
                 )
-            }.toSet() to species.id
-        }.createDedupMaps()
+              }
+              .toSet() to species.id
+        }
+        .createDedupMaps()
 
-        writer.appendLine(evoIds.entries.joinToString("\n") {
-            """
-            |static const Evolution *const $EVO_PREFIX${it.value} = (const Evolution[]) {
-            |$IND${it.key.joinToString("\n$IND") { evo -> "$evo," }}
-            |${IND}0};
-            |""".trimMargin()
-        })
+    writer.appendLine(
+      evoIds.entries.joinToString("\n") {
+          """
+          |static const Evolution *const $EVO_PREFIX${it.value} = (const Evolution[]) {
+          |$IND${it.key.joinToString("\n$IND") { evo -> "$evo," }}
+          |${IND}0};
+          |"""
+          .trimMargin()
+      }
+    )
 
-        speciesEvoIds.printLookupTable(
-            "const Evolution *const gEvolutionTable[$REAL_SPECIES_COUNT]",
-            EVO_PREFIX,
-            writer
-        )
+    speciesEvoIds.printLookupTable(
+      "const Evolution *const gEvolutionTable[$REAL_SPECIES_COUNT]",
+      EVO_PREFIX,
+      writer,
+    )
 
-        val megas = SPECIES_LIST.flatMap { it.megaList.map { mega -> mega to it.id } }.groupBy { it.first.from }
-            .mapValues { (_, values) ->
-                values.map { (mega, to) ->
-                    if (mega.hasMove()) {
-                        Evo(EvoOrFormType.EVO_MOVE_MEGA_EVOLUTION, mega.move.toString(), to)
-                    } else {
-                        Evo(EvoOrFormType.EVO_MEGA_EVOLUTION, mega.item.toString(), to)
-                    }
-                }
+    val megas =
+      SPECIES_LIST.flatMap { it.megaList.map { mega -> mega to it.id } }
+        .groupBy { it.first.from }
+        .mapValues { (_, values) ->
+          values.map { (mega, to) ->
+            if (mega.hasMove()) {
+              Evo(EvoOrFormType.EVO_MOVE_MEGA_EVOLUTION, mega.move.toString(), to)
+            } else {
+              Evo(EvoOrFormType.EVO_MEGA_EVOLUTION, mega.item.toString(), to)
             }
-        val primals = SPECIES_LIST.flatMap { it.primalList.map { primal -> primal to it.id } }.groupBy { it.first.from }
-            .mapValues { (_, values) ->
-                values.map { (primal, to) ->
-                    Evo(
-                        EvoOrFormType.EVO_PRIMAL_REVERSION,
-                        primal.item.toString(),
-                        to
-                    )
-                }
-            }
-
-        val reverseForms =
-            (NO_EGG_LIST.map { it.formShiftOf to it.id }
-                .groupBy({ it.first }, { it.second }) - SpeciesEnum.SPECIES_NONE).mapValues { (base, forms) ->
-                listOf(Evo(EvoOrFormType.EVO_FORM_SHIFT, "TRUE", base)) +
-                        forms.map {
-                            when (SPECIES_MAP[it]!!.formShiftGender) {
-                                Species.Gender.MALE -> Evo(EvoOrFormType.EVO_FORM_SHIFT_GENDER, "MON_MALE", it)
-                                Species.Gender.FEMALE -> Evo(EvoOrFormType.EVO_FORM_SHIFT_GENDER, "MON_FEMALE", it)
-                                else -> Evo(EvoOrFormType.EVO_FORM_SHIFT, "TRUE", it)
-                            }
-                        }
-            }
-
-        val allForms = NO_EGG_LIST.map {
-            val formId = if (it.hasFormShiftOf()) it.formShiftOf else it.id
-            val unmega =
-                listOfNotNull((it.megaList.map { m -> m.from } + it.primalList.map { p -> p.from }).firstOrNull()).map { from ->
-                    Evo(
-                        EvoOrFormType.EVO_UNMEGA,
-                        "TRUE",
-                        from
-                    )
-                }
-            // Unmegas are expected to come first
-            (unmega + reverseForms[formId].orEmpty() + megas[it.id].orEmpty() + primals[it.id].orEmpty()) to it.id
+          }
+        }
+    val primals =
+      SPECIES_LIST.flatMap { it.primalList.map { primal -> primal to it.id } }
+        .groupBy { it.first.from }
+        .mapValues { (_, values) ->
+          values.map { (primal, to) ->
+            Evo(EvoOrFormType.EVO_PRIMAL_REVERSION, primal.item.toString(), to)
+          }
         }
 
-        val (formIds, speciesFormIds) = allForms.createDedupMaps()
+    val reverseForms =
+      (NO_EGG_LIST.map { it.formShiftOf to it.id }.groupBy({ it.first }, { it.second }) -
+          SpeciesEnum.SPECIES_NONE)
+        .mapValues { (base, forms) ->
+          listOf(Evo(EvoOrFormType.EVO_FORM_SHIFT, "TRUE", base)) +
+            forms.map {
+              when (it.data.formShiftGender) {
+                Species.Gender.MALE -> Evo(EvoOrFormType.EVO_FORM_SHIFT_GENDER, "MON_MALE", it)
+                Species.Gender.FEMALE -> Evo(EvoOrFormType.EVO_FORM_SHIFT_GENDER, "MON_FEMALE", it)
+                else -> Evo(EvoOrFormType.EVO_FORM_SHIFT, "TRUE", it)
+              }
+            }
+        }
 
-        writer.appendLine(formIds.entries.joinToString("\n") {
-            """
-            |static const Evolution *const $FORM_PREFX${it.value} = (const Evolution[]) {
-            |$IND${it.key.joinToString("\n$IND") { evo -> "$evo," }}
-            |${IND}0};
-            |""".trimMargin()
-        })
+    val allForms =
+      NO_EGG_LIST.map {
+        val formId = if (it.hasFormShiftOf()) it.formShiftOf else it.id
+        val unmega =
+          listOfNotNull(
+              (it.megaList.map { m -> m.from } + it.primalList.map { p -> p.from }).firstOrNull()
+            )
+            .map { from -> Evo(EvoOrFormType.EVO_UNMEGA, "TRUE", from) }
+        // Unmegas are expected to come first
+        (unmega +
+          reverseForms[formId].orEmpty() +
+          megas[it.id].orEmpty() +
+          primals[it.id].orEmpty()) to it.id
+      }
 
-        speciesFormIds.printLookupTable(
-            "const Evolution *const gFormChangeTable[$REAL_SPECIES_COUNT]",
-            FORM_PREFX,
-            writer
-        )
-    }
+    val (formIds, speciesFormIds) = allForms.createDedupMaps()
+
+    writer.appendLine(
+      formIds.entries.joinToString("\n") {
+          """
+          |static const Evolution *const $FORM_PREFX${it.value} = (const Evolution[]) {
+          |$IND${it.key.joinToString("\n$IND") { evo -> "$evo," }}
+          |${IND}0};
+          |"""
+          .trimMargin()
+      }
+    )
+
+    speciesFormIds.printLookupTable(
+      "const Evolution *const gFormChangeTable[$REAL_SPECIES_COUNT]",
+      FORM_PREFX,
+      writer,
+    )
+  }
 }
