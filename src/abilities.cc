89d7991141e8d3ed3d5f811cf740ed8977f530e0
f11j <90928892+f11j@users.noreply.github.com>
2026-04-21 01:05:14 -0400

    Update Scarecrow to new effect

--- src/abilities.cc
+++ src/abilities.cc
@@ -9979,13 +9979,7 @@ constexpr IntimidateCloneData Intimidate<ABILITY_SCARECROW> = Intimidate<ABILITY
 template <>
 constexpr Ability Impl<ABILITY_SCARECROW> = {
     .onEntry = UseIntimidateClone,
-    .onAccuracy = Impl<ABILITY_BAD_LUCK>.onAccuracy,
-    .onCrit = Impl<ABILITY_BAD_LUCK>.onCrit,
-    .onModifyEffectChance = Impl<ABILITY_BAD_LUCK>.onModifyEffectChance,
-    .onAccuracyFor = Impl<ABILITY_BAD_LUCK>.onAccuracyFor,
-    .onCritFor = Impl<ABILITY_BAD_LUCK>.onCritFor,
-    .onModifyEffectChanceFor = Impl<ABILITY_BAD_LUCK>.onModifyEffectChanceFor,
-    .breakable = TRUE,
+    .onDefender = Impl<ABILITY_COTTON_DOWN>.onDefender,
 };
 
 template <>
